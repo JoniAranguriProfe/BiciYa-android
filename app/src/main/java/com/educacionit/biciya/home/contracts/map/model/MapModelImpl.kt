777121f6d8146829dcbc09ce3e5a9c54e88b0229
Jonathan Aranguri <arangurim@gmail.com>
2025-10-06 20:10:25 -0300

    feat: Move the clientSecret and clientId to EcoBiciService

--- app/src/main/java/com/educacionit/biciya/home/contracts/map/model/MapModelImpl.kt
+++ app/src/main/java/com/educacionit/biciya/home/contracts/map/model/MapModelImpl.kt
@@ -7,7 +7,7 @@ import com.educacionit.biciya.network.EcoBiciService
 class MapModelImpl(private val ecoBiciService: EcoBiciService) : MapModel {
     override suspend fun getStations(): StationInformationResponse? {
         val response =
-            ecoBiciService.getStationInformation(BuildConfig.CLIENT_ID, BuildConfig.CLIENT_SECRET)
+            ecoBiciService.getStationInformation()
         return response.body()
     }
 }
--- app/src/main/java/com/educacionit/biciya/network/EcoBiciService.kt
+++ app/src/main/java/com/educacionit/biciya/network/EcoBiciService.kt
@@ -1,5 +1,6 @@
 package com.educacionit.biciya.network
 
+import com.educacionit.biciya.BuildConfig
 import com.educacionit.biciya.models.response.StationInformationResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -8,6 +9,8 @@ import retrofit2.http.Query
 interface EcoBiciService {
 
     @GET("stationInformation")
-    suspend fun getStationInformation(@Query("client_id") clientId: String,
-                                      @Query("client_secret") clientSecret: String): Response<StationInformationResponse>
+    suspend fun getStationInformation(
+        @Query("client_id") clientId: String = BuildConfig.CLIENT_ID,
+        @Query("client_secret") clientSecret: String = BuildConfig.CLIENT_SECRET
+    ): Response<StationInformationResponse>
 }
